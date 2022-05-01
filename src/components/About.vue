649f0e41989eb77e78b4117bf84f694b95a89435
Henrique Nascimento <henriquenascandrade@gmail.com>
2022-05-01 16:45:42 -0300

    add styles and logo ourikas

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,22 @@
 <template>
     <div class="container">
-        <h1 class="title">Ourikas em Vuejs 3</h1>
+        <div class="has-background-primary m-5 p-5">
+            <div class="columns is-flex is-centered">
+                <img src="https://avatars.githubusercontent.com/u/7742471?s=200&v=4">
+            </div>
+
+            <h1 class="title has-text-white is-size-2 has-text-centered has-text-weight-bold">Ourikas em Vuejs 3</h1>
+            <hr>
+            <p class="subtitle bold has-text-white has-text-weight-bold">Aplicação criada com fins de estudo para conhecer as principais features do Vuejs 3.
+                Estou consumindo uma API hospedada no github
+                <a class="tag is-medium is-info is-light" href="http://ourikas.github.io/companies.json" target="_blank">Ourikas</a>
+                que me retorna um JSON.
+            </p>
+
+            <p class="subtitle has-text-white has-text-weight-bold">Agradecimento a
+                <a class="tag is-medium is-info is-light" href="https://github.com/mauriciomelo" target="_blank">mauriciomelo</a>
+                por disponibilizar esse serviço de forma pública para que possamos aplicar em testes e estudos.
+            </p>
+        </div>
     </div>
 </template>
