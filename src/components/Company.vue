c2edaefc079c1b497a8c9ccb062c6b29401a466f
Henrique Nascimento <henriquenascandrade@gmail.com>
2022-05-01 16:46:16 -0300

    add styles and more informations

--- src/components/Company.vue
+++ src/components/Company.vue
@@ -1,27 +1,50 @@
 <template>
-    <div class="container">
+    <div class="container card m-5 p-5">
         <div v-for="comp in company" :key="comp.id">
-            <h2>{{ comp.name }}</h2>
-            <span>{{ comp.status ? 'Ativa' : 'Inativa' }}</span>
+            <span class="is-size-1 mr-4">{{ comp.name }}</span>
+            <span :class="comp.status ? 'tag is-primary is-medium' : 'tag is-warning is-medium' ">{{ comp.status ? 'Ativa' : 'Inativa' }}</span>
             <br>
-            <span>{{ comp.contact.email }}</span>
-            <p>{{ comp.description }}</p>
+            <p class="subtitle">{{ comp.description }}</p>
             <ul>
                 <li>{{ comp.keywords }}</li>
             </ul>
 
-            <h3>Contatos:</h3>
-            <ul>
-                <li v-for="(contact, index) in comp.contact.phones" :key="index">
-                    {{ contact }}
-                </li>
-                <li>{{ comp.contact.site }}</li>
-                <li>{{ comp.contact.facebook }}</li>
-            </ul>
+            <br>
 
-            <h3>Endereço</h3>
-            <p>{{ comp.address.street }} | CEP: {{ comp.address.zip }}</p>
-            <p>{{ comp.address.city }} / {{ comp.address.state }}</p>
+            <h3 class="tag is-primary is-size-5 has-text-weight-bold mb-3">Contatos:</h3>
+            <div class="ml-2">
+                
+                <span class="subtitle">e-mail: {{ comp.contact.email }}</span>
+                <br>
+                <span class="subtitle">
+                    site:
+                    <a :href="comp.contact.site" target="_blank">
+                        acessar site
+                    </a>
+                </span>
+                <br>
+                <span class="subtitle">
+                    facebook: 
+                    <a :href="comp.contact.facebook" target="_blank">
+                        página no facebook
+                    </a>
+                </span>
+                <br><br>
+                <span class="subtitle">telefones: </span>
+                <span class="subtitle" v-for="(contact, index) in comp.contact.phones" :key="index">
+                    {{ contact }}
+                </span>
+            </div>
+
+            <br>
+
+            <h3 class="tag is-primary is-size-5 has-text-weight-bold mb-3">Endereço</h3>
+            <br>
+            <div class="ml-2">
+                <span class="subtitle">{{ comp.address.street }} | CEP: {{ comp.address.zip }}</span>
+                <br>
+                <span class="subtitle">{{ comp.address.city }} / {{ comp.address.state }}</span>
+            </div>
         </div>
     </div>
 </template>
