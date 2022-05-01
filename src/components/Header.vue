6228fde9c3585e71b884b8cad90b8cc22d62d88f
Henrique Nascimento <henriquenascandrade@gmail.com>
2022-05-01 16:46:56 -0300

    create header and add links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,49 +1,53 @@
 <template>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
-<nav class="navbar mb-6" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <a class="navbar-item" href="https://bulma.io">
-      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-    </a>
+<div class="container">
 
-    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
+  <nav class="navbar mb-6" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+      </a>
 
-  <div id="navbarBasicExample" class="navbar-menu">
-    <div class="navbar-start is-centered">
-      <router-link to="/" class="navbar-item">
-        Home
-      </router-link>
-
-      <router-link to="/companies" class="navbar-item">
-        Lista de empresas
-      </router-link>
-      
-      <router-link to="/about" class="navbar-item">
-        Ourikas Vue
-      </router-link>
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
     </div>
 
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start is-centered">
+        <router-link to="/" class="navbar-item">
+          Home
+        </router-link>
 
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="buttons">
-          <a class="button is-primary">
-            <strong>Sign up</strong>
-          </a>
-          <a class="button is-light">
-            Log in
-          </a>
-        </div>
+        <router-link to="/companies" class="navbar-item">
+          Lista de empresas
+        </router-link>
+        
+        <router-link to="/about" class="navbar-item">
+          Ourikas Vue
+        </router-link>
       </div>
+
+
+      <!-- <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Sign up</strong>
+            </a>
+            <a class="button is-light">
+              Log in
+            </a>
+          </div>
+        </div>
+      </div> -->
     </div>
-  </div>
-</nav>
-<router-view />
+  </nav>
+  <router-view />
+
+</div>
 </template>
 
 <script>
