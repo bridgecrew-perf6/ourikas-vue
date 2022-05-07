da94cda804e21a9500646929304d74fad3f3fc32
Henrique Nascimento <henriquenascandrade@gmail.com>
2022-05-07 19:38:39 -0300

    Fix styles details company (buttons, tags and description)

--- src/components/Company.vue
+++ src/components/Company.vue
@@ -1,48 +1,48 @@
 <template>
-    <div class="container my-10 mx-auto px-4 md:px-2">
+    <div class="container my-10 mx-auto">
         <div v-for="comp in company" :key="comp.id" class="p-5 text-cyan-900 bg-white rounded-lg border border-gray-200 shadow-md">
-            <div class="mb-2">
-                <span class="font-bold text-5xl">{{ comp.name }}</span>
-                <span :class="comp.status ? '' : '' ">{{ comp.status ? 'Ativa' : 'Inativa' }}</span>
+            <div class="flex justify-between">
+                <div class="text-6xl font-bold">{{ comp.name }}</div>
+                <div class="px-5 h-6 content-center text-white rounded-full" :class="comp.status ? 'bg-green-400' : 'bg-gray-500'">{{ comp.status ? 'ativa' : 'inativa' }}</div> 
             </div>
             <br>
-            <p class="">{{ comp.description }}</p>
-            <ul v-if="comp.keywords.length > 0">
-                <li class="" v-for="(tag, index) in splitTags(comp.keywords)" :key="index">{{ tag }}</li>
-            </ul>
+            <div class="">
+                <p class="text-3xl mb-3">{{ comp.description }}</p>
+                <ul v-if="comp.keywords.length > 0" class="flex">
+                    <li v-for="(tag, index) in splitTags(comp.keywords)" :key="index" class="bg-gray-900 px-5 h-7 content-center text-white rounded-full mx-1">{{ tag }}</li>
+                </ul>
+            </div>
 
             <br>
 
-            <h3 class="">Contatos:</h3>
+            <h3 class="text-3xl font-bold mb-3">Contatos</h3>
             <div class="">
-                <span class="" v-if="comp.contact.email">{{ comp.contact.email }}</span>
+                <div class="flex space-x-3">
+                    <div class="bg-blue-500 font-semibold text-white py-2 px-4 rounded" v-if="comp.contact.email">{{ comp.contact.email }}</div>
                 
-                <span v-if="comp.contact.site">
-                    <a :href="comp.contact.site" target="_blank" class="">
+                    <a class="bg-gray-800 font-semibold text-white py-2 px-4 rounded" :href="comp.contact.site" target="_blank" v-if="comp.contact.site">
                         acessar site
                     </a>
-                </span>
-                
-                <span v-if="comp.contact.facebook">
-                    <a :href="comp.contact.facebook" target="_blank" class="">
+                    
+                    <a :href="comp.contact.facebook" target="_blank" class="bg-blue-800 font-semibold text-white py-2 px-4 rounded" v-if="comp.contact.facebook">
                         página no facebook
                     </a>
-                </span>
-                <br><br>
-                <span class="" v-for="(contact, index) in comp.contact.phones" :key="index">
-                    {{ contact }}
-                </span>
+                    <br>
+                    <div class="bg-cyan-800 font-semibold text-white py-2 px-4 rounded" v-for="(contact, index) in comp.contact.phones" :key="index">
+                        {{ contact }}
+                    </div>
+                </div>
             </div>
 
             <br>
 
-            <h3 class="">Endereço</h3>
+            <h3 class="text-3xl font-bold mb-3">Endereço</h3>
             <div class="">
-                <span class="" v-show="comp.address.street">{{ comp.address.street }}</span>
+                <span class="text-2xl" v-show="comp.address.street">{{ comp.address.street }}</span>
                 <br>
-                <span class="" v-show="comp.address.zip">CEP: {{ comp.address.zip }}</span>
+                <span class="text-2xl" v-show="comp.address.zip">CEP: {{ comp.address.zip }}</span>
                 <br>
-                <span class="" v-show="comp.address.city">{{ comp.address.city }} / {{ comp.address.state }}</span>
+                <span class="text-2xl" v-show="comp.address.city">{{ comp.address.city }} / {{ comp.address.state }}</span>
             </div>
         </div>
     </div>
