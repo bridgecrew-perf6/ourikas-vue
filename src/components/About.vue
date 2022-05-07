882e10e3d2623436eae6fdffe52efa178e74994c
Henrique Nascimento <henriquenascandrade@gmail.com>
2022-05-07 20:16:31 -0300

    fix styles about component

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,20 +1,20 @@
 <template>
-    <div class="container">
-        <div class="has-background-primary m-5 p-5">
-            <div class="columns is-flex is-centered">
-                <img src="../assets/images/logo-ourikas-white.png" width="200">
+    <div class="container max-w-6xl mt-10">
+        <div class="card bg-white rounded-lg border border-gray-200 shadow-md p-8">
+            <div class="flex justify-center">
+                <img src="../assets/images/logo-ourikas-green.png" width="200">
             </div>
 
-            <h1 class="title has-text-white is-size-2 has-text-centered has-text-weight-bold">Ourikas em Vuejs 3</h1>
+            <h1 class="text-6xl text-center font-bold text-cyan-800 mb-4">Ourikas - Vuejs 3</h1>
             <hr>
-            <p class="subtitle bold has-text-white has-text-weight-bold">Aplicação criada com fins de estudo para conhecer as principais features do Vuejs 3.
+            <p class="text-2xl text-gray-800 mb-4">Aplicação criada com fins de estudo para conhecer as principais features do Vuejs 3.
                 Estou consumindo uma API hospedada no github
-                <a class="tag is-medium is-info is-light" href="http://ourikas.github.io/companies.json" target="_blank">Ourikas</a>
+                <a class="bg-transparent px-2 content-center font-bold underline text-cyan-800 rounded-lg hover:bg-cyan-800 hover:text-white" href="http://ourikas.github.io/companies.json" target="_blank">Ourikas</a>
                 que me retorna um JSON.
             </p>
 
-            <p class="subtitle has-text-white has-text-weight-bold">Agradecimento a
-                <a class="tag is-medium is-info is-light" href="https://github.com/mauriciomelo" target="_blank">mauriciomelo</a>
+            <p class="text-2xl text-gray-800">Agradecimento a
+                <a class="bg-transparent px-2 content-center font-bold underline text-cyan-800 rounded-lg hover:bg-cyan-800 hover:text-white" href="https://github.com/mauriciomelo" target="_blank">mauriciomelo</a>
                 por disponibilizar esse serviço de forma pública para que possamos aplicar em testes e estudos.
             </p>
         </div>
